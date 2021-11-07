f0fe73658d420322ec4344e9b3fdb424cacd0ef6
cactusnoh <cactusnoh@gmail.com>
2021-11-07 22:19:09 +0900

    Deleted files and comments

--- 6.006/peak_finder.cc
+++ 6.006/peak_finder.cc
@@ -1,17 +1,6 @@
 #include <iostream>
 #include <vector>
 
-/*
-0 1 2 3 4 5 6 7 8 9
-
-0
-
-0 1
-
-0 1 2 3 4 5 4 3 2 1
-
-*/
-
 int peak_finder_binary(std::vector<int> numbers)
 {
     int left = 0, right = numbers.size() - 1;
